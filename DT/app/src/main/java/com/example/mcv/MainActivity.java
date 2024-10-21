d915b38bea21f96c7e158cffbd6843f371eed8c7
LuccaFSAF <lucca.fsa05@aluno.ifsc.edu.br>
2024-10-21 12:06:04 -0300

    Semi-Finalizado

--- DT/app/src/main/java/com/example/mcv/MainActivity.java
+++ DT/app/src/main/java/com/example/mcv/MainActivity.java
@@ -3,21 +3,26 @@ package com.example.mcv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase database;
     EditText edtNome, edtEmail, edtData;
     Button button;
+    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         edtData = findViewById(R.id.editTextDT);
         //Definindo tratamento para evento de click
         button = findViewById(R.id.btnCadastrar);
+        listview = findViewById(R.id.listView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,10 +50,37 @@ public class MainActivity extends AppCompatActivity {
                 }else {
                     Toast.makeText(getApplicationContext(), "Erro na inserção", Toast.LENGTH_SHORT).show();
                 }
+                carregarListagem();
+                edtNome.setText("");
+                edtEmail.setText("");
+                edtData.setText("");
+            }
+        });
+
+        listview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email;
+                
             }
         });
 
         database = openOrCreateDatabase("meubd", MODE_PRIVATE, null);
         database.execSQL("create table if not exists pessoas(id INTEGER primary key autoincrement,nome varchar, email varchar, dtnasc DATE)");
+        carregarListagem();
+    }
+    public void carregarListagem() {
+        Cursor cursor = database.rawQuery("Select * from pessoas", null);
+        cursor.moveToFirst();
+        ArrayList<String> nomes = new ArrayList<>(0);
+        while (!cursor.isAfterLast()) {
+            nomes.add(cursor.getString(1));
+            cursor.moveToNext();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes);
+        listview.setAdapter(adapter);
     }
 }
